e6e51d43ef034494b200532abd7f3f8f970bda1c
ihuomagold <ihuomagold53@gmail.com>
2020-12-17 21:13:52 +0100

    updated utils.py, functions and main

--- capstone project/utils.py
+++ capstone project/utils.py
@@ -1,27 +1,24 @@
 import sqlite3
-# This creates a connection to the Database
 
 
-def connect_to_database(db_name):
+def connect_to_database(db_name):     # This creates a connection to the Database
 
     conn = sqlite3.connect(db_name)
 
-    cursor = conn.cursor()
-
-    return cursor
+    return conn
 
 
-def create_table(cursor, table_name, table_properties):
+def create_table(cursor, table_name, table_properties):   # Creates a table in the Database
     cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name}({table_properties})")
 
 
-def add_users_to_table(cursor, table_name, columns, values):
+def add_users_to_table(cursor, table_name, columns, values):   # Adds Users to the Database
     cursor.execute(f"INSERT INTO {table_name} ({columns}) VALUES({values})")
 
 
-def delete_users(cursor, table_name, identity):
+def delete_users(cursor, table_name, identity):  # Deletes Users from the database
     cursor.execute(f"DELETE FROM {table_name} WHERE rowid = ({identity})")
 
 
-def update_user(cursor, table_name, columns, values, identity):
+def update_user(cursor, table_name, columns, values, identity):  # Updates a user record
     cursor.execute(f"UPDATE {table_name} SET {columns} = {values} WHERE rowid = ({identity})")
